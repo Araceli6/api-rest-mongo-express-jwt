07ae9bb566ed3d95bf30358267c0fc9bfd150591
Araceli6 <arachavez1901@gmail.com>
2024-02-15 15:17:26 -0600

    listo

---
+++ database/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+try {
+    await mongoose.connect(process.env.URI_MONGO)
+    console.log('Connect DB ok👍')
+} catch (error) {
+    console.log('Error de conexión a mongodb😣' + error)
+}
